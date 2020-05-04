aa8e727d984ab17651297c8c4fb0434a968d8d94
Tom Nielsen <tanielsen@gmail.com>
2020-05-04 21:46:49 +0100

    initial

---
+++ json.js
@@ -0,0 +1,32 @@
+const { textarea, text, code, pre } = require("saltcorn-markup/tags");
+
+const json = {
+  name: "JSON",
+  sql_name: "jsonb",
+  fieldviews: {
+    show: { isEdit: false, run: v => pre({ class: "wsprewrap" }, code(JSON.stringify(v))) },
+    edit: {
+      isEdit: true,
+      run: (nm, v, attrs, cls) =>
+        textarea(
+          {
+            class: ["form-control", cls],
+            name: text(nm),
+            id: `input${text(nm)}`,
+            rows: 10
+          },
+          text(v) || ""
+        )
+    }
+  },
+  read: v => {
+    switch (typeof v) {
+      case "string":
+        return JSON.parse(v);
+      default:
+        return v;
+    }
+  }
+};
+
+module.exports = { types: [json] };
