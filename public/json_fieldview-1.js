06db0dd304c84a5cbedb66728cbae55f635451c7
Tom Nielsen <tanielsen@gmail.com>
2022-01-07 16:07:06 +0000

    input to match key on new / changed rows

--- public/json_fieldview-1.js
+++ public/json_fieldview-1.js
@@ -12,11 +12,20 @@ function jsonTableEdit(nm) {
     // reference all the stuff you need first
     const $row = $(row);
     const k = $row.find("input.json_key,select.json_key").val();
-    const v =
-      schemaMap && schemaMap[k]?.type === "Bool"
-        ? $row.find("input.json_value").prop("checked")
-        : $row.find("input.json_value").val();
-    obj[k] = v;
+    const velem = $row.find("input.json_value");
+    if (schemaMap && schemaMap[k] && schemaMap[k].type === "Bool") {
+      obj[k] = velem.prop("checked");
+      if (velem.attr("type") !== "checkbox")
+        velem.replaceWith(
+          `<input type="checkbox" class="json_value" onchange="jsonTableEdit('${nm}')">`
+        );
+    } else {
+      obj[k] = velem.val();
+      if (velem.attr("type") === "checkbox")
+        velem.replaceWith(
+          `<input type="text" class="json_value" onchange="jsonTableEdit('${nm}')" value="">`
+        );
+    }
   });
   const s = JSON.stringify(obj);
   $(`#input${nm}`).val(s);
@@ -24,19 +33,20 @@ function jsonTableEdit(nm) {
 
 function jsonTableAddRow(nm) {
   const schemaMap = getSchemaMap(nm);
-
+  const schemaKeys = schemaMap && Object.keys(schemaMap);
   const keyInput = schemaMap
     ? `<select class="json_key" onchange="jsonTableEdit('${nm}')">
-        ${Object.keys(schemaMap)
-          .map((k) => `<option>${k}</option>`)
-          .join("")}
+        ${schemaKeys.map((k) => `<option>${k}</option>`).join("")}
        </select>`
     : `<input type="text" class="json_key" onchange="jsonTableEdit('${nm}')" value="">`;
-  //const valInput = schemaMap ? `` : ``;
+  const valInput =
+    schemaMap && schemaMap[schemaKeys[0]].type === "Bool"
+      ? `<input type="checkbox" class="json_value" onchange="jsonTableEdit('${nm}')">`
+      : `<input type="text" class="json_value" onchange="jsonTableEdit('${nm}')" value="">`;
   $(`#table-edit-${nm}`).append(`
   <tr>
     <td>${keyInput}</td>
-    <td><input type="text" class="json_value" onchange="jsonTableEdit('${nm}')" value=""></td>
+    <td>${valInput}</td>
     <td><i class="fas fa-times" onclick="jsonTableDeleteRow('${nm}', this)"></i></td>
   </tr>
   `);
